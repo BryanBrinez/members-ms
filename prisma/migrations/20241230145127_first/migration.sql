ab05aadc261c599cd6081c53ef58e2b2ceae0f41
Bryan Brinez <bryan-26-8@hotmail.com>
2024-12-30 11:27:45 -0500

    prisma updated

--- prisma/migrations/20241230145127_first/migration.sql
+++ prisma/migrations/20241230145127_first/migration.sql
@@ -1,8 +1,12 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'MIEMBRO');
+
 -- CreateTable
 CREATE TABLE "Member" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "projectsId" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
 
     CONSTRAINT "Member_pkey" PRIMARY KEY ("id")
 );
